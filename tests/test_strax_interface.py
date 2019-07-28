6cd6b6992070f488c773de6a65b8dba2df4023c5
Jelle Aalbers <jelleaalbers@gmail.com>
2019-07-28 12:45:23 +0200

    Cache pip, test in temporary dir
    (until strax problem with multi-output plugins when not saving is solved)

--- tests/test_strax_interface.py
+++ tests/test_strax_interface.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import strax
 import straxen
 import wfsim
@@ -8,14 +10,14 @@ strax.mailbox.Mailbox.DEFAULT_TIMEOUT = 20
 def test_sim():
     # Just some id from post-SR1, so the corrections work
     run_id = '180519_1902'
+    with tempfile.TemporaryDirectory() as tempdir:
+        st = strax.Context(
+            storage=tempdir,
+            register=wfsim.RawRecordsFromFax,
+            config=dict(nevents=4),
+            **straxen.contexts.common_opts)
 
-    st = strax.Context(
-        storage=[],
-        register=wfsim.RawRecordsFromFax,
-        config=dict(nevents=4),
-        **straxen.contexts.common_opts)
-
-    rr = st.get_array(run_id, 'raw_records')
+        rr = st.get_array(run_id, 'raw_records')
 
     assert len(rr) > 0
     assert rr['data'].sum() > 0
