d03c9f3914899c44ec714cc69b09bd2959483250
Tianyu Zhu <tz2263@columbia.edu>
2020-03-11 11:39:45 -0500

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='wfsim',
-    version='0.0.3',
+    version='0.0.4',
     description='XENONnT Waveform simulator',
     author='Wfsim contributors, the XENON collaboration',
     url='https://github.com/XENONnT/wfsim',
