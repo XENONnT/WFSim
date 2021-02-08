4d893910504d1fcca0278e5bd27e800cfd28e63d
Peter Gaemers <petergaemers@hotmail.com>
2021-02-08 16:28:57 +0100

    Updates

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='wfsim',
-    version='0.1.0',
+    version='0.1.1',
     description='XENONnT Waveform simulator',
     author='Wfsim contributors, the XENON collaboration',
     url='https://github.com/XENONnT/wfsim',
