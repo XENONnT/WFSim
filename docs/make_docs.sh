bcde69a88fbb5ce6e9c72bb7b62187a92d761999
Joran Angevaare <jorana@nikhef.nl>
2021-02-27 12:16:46 +0100

    cleanup makedocs

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -3,10 +3,6 @@ make clean
 rm -r source/reference
 rm source/tutorials/*
 cd source/tutorials
-#for notebook in Understanding_WFSim_P1.ipynb Understanding_WFSim_P2.ipynb Advanced_tricks.ipynb;
-#  do
-#    ln -s ../../../notebooks/$notebook .
-#  done
 ln -s ../../../notebooks/* .
 cd ../..
 sphinx-apidoc -o source/reference ../wfsim
