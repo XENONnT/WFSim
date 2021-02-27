b847f3d53efe420a37030a41115b4beb90a98f2e
Joran Angevaare <jorana@nikhef.nl>
2021-02-27 11:48:50 +0100

    add docs

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -3,10 +3,11 @@ make clean
 rm -r source/reference
 rm source/tutorials/*
 cd source/tutorials
-for notebook in Understanding_WFSim_P1.ipynb Understanding_WFSim_P2.ipynb Advanced_tricks.ipynb;
-  do
-    ln -s ../../../notebooks/$notebook .
-  done
+#for notebook in Understanding_WFSim_P1.ipynb Understanding_WFSim_P2.ipynb Advanced_tricks.ipynb;
+#  do
+#    ln -s ../../../notebooks/$notebook .
+#  done
+ln -s ../../../notebooks/* .
 cd ../..
 sphinx-apidoc -o source/reference ../wfsim
 rm source/reference/modules.rst
