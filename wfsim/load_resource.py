1ccc71c4b988182a7d74400b87a3428a14b14dd6
Tianyu Zhu <tz2263@columbia.edu>
2021-03-11 15:03:02 -0600

    replace dummy map with function

--- wfsim/load_resource.py
+++ wfsim/load_resource.py
@@ -26,15 +26,14 @@ def load_config(config):
 class Resource:
     """
     Get the configs needed for running WFSim. Configs can be obtained in
-        three ways:
+        two ways:
         1. Get it directly from the mongo database. This only needs the
             name of the file.
-        2. Get if from the 'ntauxfiles' package. This should only be
-            used by developers because you might get a version that is
-            outdated.
-        3. Download it from the strax_auxiliary_files repository. Since
-            this is a public repository anyone can download the files
-            stored here.
+        2. Load it with straxen get_resource, this can either:
+            Download from a public repository
+            Read from local cache
+            Download from a private repository if credentials 
+            are properly setup
     """
     def __init__(self, config=None):
         log.debug(f'Getting {config}')
@@ -68,11 +67,12 @@ class Resource:
         else:
             raise ValueError(f"Unsupported detector {config['detector']}")
 
+        # Allowing user to replace default with specified files
         for k in set(config).intersection(files):
-            files[k] = config[k]  # Allowing user to replace default with specified files
+            files[k] = config[k]
 
         commit = 'master'  # Replace this by a commit hash if you feel solid and responsible
-        if getattr(config, 'url_base', False):
+        if config.get('url_base', False):
             url_base = config['url_base']
         elif config['detector'] == "XENON1T":
             url_base = f'https://raw.githubusercontent.com/XENONnT/strax_auxiliary_files/{commit}/sim_files'
@@ -80,13 +80,16 @@ class Resource:
             url_base = f'https://raw.githubusercontent.com/XENONnT/WFSim/{commit}/files'
 
         for k, v in files.items():
+            if isinstance(v, list):
+                continue
             log.debug(f'Obtaining {k} from {v}')
             if v.startswith('/'):
                 log.warning(f"WARNING: Using local file {v} for a resource. "
                             f"Do not set this as a default or TravisCI tests will break")
             try:
                 # First try downloading it via
-                # https://straxen.readthedocs.io/en/latest/config_storage.html#downloading-xenonnt-files-from-the-database  # noqa
+                # https://straxen.readthedocs.io/en/latest/config_storage.html
+                # downloading-xenonnt-files-from-the-database  # noqa
 
                 # we need to add the straxen.MongoDownloader() in this
                 # try: except NameError: logic because the NameError
@@ -111,6 +114,7 @@ class Resource:
                     log.warning(f'{k} did not download, trying {raw_url}')
                     files[k] = raw_url
             log.debug(f'Downloaded {k} successfully')
+
         self.photon_area_distribution = straxen.get_resource(files['photon_area_distribution'], fmt='csv')
 
         if config['detector'] == 'XENON1T':
@@ -119,30 +123,36 @@ class Resource:
             self.s2_light_yield_map = make_map(files['s2_light_yield_map'], fmt='json')
             self.s2_pattern_map = make_map(files['s2_pattern_map'], fmt='json.gz')
             self.fdc_3d = make_map(files['fdc_3d'], fmt='json.gz')
-            # TODO
-            # config not set
-            self.gas_gap_length = lambda positions: np.ones(253)
 
             # Gas gap warping map
             if config['enable_gas_gap_warping']:
-                gas_gap_map = straxen.get_resource(files['gas_gap_map'], fmt='pkl')
-                self.gas_gap_length = lambda positions: np.ones(253)
+                self.gas_gap_length = make_map(["constant dummy", 1, [254,]])
 
         if config['detector'] == 'XENONnT':
             self.s1_pattern_map = make_map(files['s1_pattern_map'], fmt='pkl')
-            lymap = deepcopy(self.s1_pattern_map)
-            lymap.data['map'] = np.sum(lymap.data['map'][:][:][:], axis=3, keepdims=True)
-            lymap.__init__(lymap.data)
-            self.s1_light_yield_map = lymap
+            if isinstance(self.s1_pattern_map, DummyMap):
+                self.s1_light_yield_map = self.s1_pattern_map.reduce_last_dim()
+            else:
+                lymap = deepcopy(self.s1_pattern_map)
+                lymap.data['map'] = np.sum(lymap.data['map'][:][:][:], axis=3, keepdims=True)
+                lymap.__init__(lymap.data)
+                self.s1_light_yield_map = lymap
 
             self.s2_pattern_map = make_map(files['s2_pattern_map'], fmt='pkl')
-            lymap = deepcopy(self.s2_pattern_map)
-            lymap.data['map'] = np.sum(lymap.data['map'][:][:], axis=2, keepdims=True)
-            lymap.__init__(lymap.data)
-            self.s2_light_yield_map = lymap
-            self.s2_luminescence = straxen.get_resource(files['s2_luminescence'], fmt='pkl.gz')
-            self.fdc_3d = dummy_map(result=0)
-            
+            if isinstance(self.s2_pattern_map, DummyMap):
+                self.s2_light_yield_map = self.s2_pattern_map.reduce_last_dim()
+            else:
+                lymap = deepcopy(self.s2_pattern_map)
+                lymap.data['map'] = np.sum(lymap.data['map'][:][:], axis=2, keepdims=True)
+                lymap.__init__(lymap.data)
+                self.s2_light_yield_map = lymap
+
+            if config['s2_luminescence_model']:
+                self.s2_luminescence = straxen.get_resource(files['s2_luminescence'], fmt='pkl.gz')
+
+            if config['field_distortion_on']:
+                self.fdc_3d = make_map(files['fdc_3d'], fmt='json.gz')
+
             # Gas gap warping map
             if config['enable_gas_gap_warping']:
                 gas_gap_map = straxen.get_resource(files['gas_gap_map'], fmt='pkl')
@@ -167,15 +177,42 @@ class Resource:
         if config['neutron_veto']:
             self.nv_pmt_qe_data = straxen.get_resource(files['nv_pmt_qe_file'], fmt='json')
 
-    log.debug(f'{self.__class__.__name__} fully initialized')
+        log.debug(f'{self.__class__.__name__} fully initialized')
 
-def make_map(map_file: str, fmt='text'):
-    map_data = straxen.get_resource(map_file, fmt=fmt)
-    return straxen.InterpolatingMap(map_data)
+def make_map(map_file, fmt='text'):
+    """
+    Fetch and make an instance of InterpolatingMap based on map_file
 
+    Alternativly map_file can be a list of ["constant dummy", constant: int, shape: list]
+        return an instance of  DummyMap
+    """
+    if isinstance(map_file, list):
+        assert map_file[0] == 'constant dummy', ('Alternative file input can only be '
+            '("constant dummy", constant: int, shape: list')
+        return DummyMap(map_file[1], map_file[2])
 
-class dummy_map():
-    def __init__(self, result):
-        self.result = result
-    def __call__(self, positions):
-        return np.ones(positions.shape[0]) * self.result
+    elif isinstance(map_file, str):
+        map_data = straxen.get_resource(map_file, fmt=fmt)
+        return straxen.InterpolatingMap(map_data)
+    
+    else:
+        raise TypeError("Can't handle map_file except a string or a list")
+
+class DummyMap():
+    """Return constant results
+        the length match the length of input
+        but from the second dimensions the shape is user defined input
+    """
+    def __init__(self, const, shape=()):
+        self.const = const
+        self.shape = shape
+    def __call__(self, x):
+        shape = [len(x)] + list(self.shape)
+        return np.ones(shape) * self.const
+    def reduce_last_dim(self):
+        assert len(self.shape) >= 1, 'Need at least 1 dim to reduce further'
+        const = self.const * self.shape[-1]
+        shape = list(self.shape)
+        shape[-1] = 1
+
+        return DummyMap(const, shape)
