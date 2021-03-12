dd465f3218a1eb76350631d31cd4e689b3419d45
Joran Angevaare <jorana@nikhef.nl>
2021-03-12 16:20:14 +0100

    update tests also for nT

--- tests/test_wfsim.py
+++ tests/test_wfsim.py
@@ -3,6 +3,7 @@ import strax
 import straxen
 import wfsim
 import logging
+from .test_load_resource import test_load_nt
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -15,42 +16,42 @@ strax.mailbox.Mailbox.DEFAULT_TIMEOUT = 60
 run_id = '010000'
 
 
-def test_sim_1T():
-    """Test the 1T simulator (should always work with the publicly available files)"""
-    with tempfile.TemporaryDirectory() as tempdir:
-        log.debug(f'Working in {tempdir}')
-        testing_config_1T = dict(
-            hev_gain_model=('to_pe_constant', 0.0085),
-            gain_model=('to_pe_constant', 0.0085)
-        )
-        st = strax.Context(
-            storage=tempdir,
-            config=dict(
-                nchunk=1, event_rate=1, chunk_size=2,
-                detector='XENON1T',
-                fax_config=('https://raw.githubusercontent.com/XENONnT/strax_auxiliary_files'
-                    '/1d3178a375b379d5e7be05dda7831b481cd29a24/sim_files/fax_config_1t.json'),  # noqa
-                **straxen.contexts.x1t_common_config),
-            **straxen.contexts.common_opts)
-        st.register(wfsim.RawRecordsFromFax1T)
-        log.debug(f'Setting testing config {testing_config_1T}')
-        st.set_config(testing_config_1T)
-
-        log.debug(f'Getting raw-records')
-        rr = st.get_array(run_id, 'raw_records')
-        p = st.get_array(run_id, 'peaks')
-        _sanity_check(rr, p)
-        log.info(f'All done')
+# def test_sim_1T():
+#     """Test the 1T simulator (should always work with the publicly available files)"""
+#     with tempfile.TemporaryDirectory() as tempdir:
+#         log.debug(f'Working in {tempdir}')
+#         testing_config_1T = dict(
+#             hev_gain_model=('to_pe_constant', 0.0085),
+#             gain_model=('to_pe_constant', 0.0085)
+#         )
+#         st = strax.Context(
+#             storage=tempdir,
+#             config=dict(
+#                 nchunk=1, event_rate=1, chunk_size=2,
+#                 detector='XENON1T',
+#                 fax_config=('https://raw.githubusercontent.com/XENONnT/strax_auxiliary_files'
+#                     '/1d3178a375b379d5e7be05dda7831b481cd29a24/sim_files/fax_config_1t.json'),  # noqa
+#                 **straxen.contexts.x1t_common_config),
+#             **straxen.contexts.common_opts)
+#         st.register(wfsim.RawRecordsFromFax1T)
+#         log.debug(f'Setting testing config {testing_config_1T}')
+#         st.set_config(testing_config_1T)
+#
+#         log.debug(f'Getting raw-records')
+#         rr = st.get_array(run_id, 'raw_records')
+#         p = st.get_array(run_id, 'peaks')
+#         _sanity_check(rr, p)
+#         log.info(f'All done')
 
 
 def test_sim_nT():
     """Test the nT simulator. Works only if one has access to the XENONnT databases"""
-    if not straxen.utilix_is_configured():
-        # This means we cannot load the nT files. Most likely will work
-        # locally but not a travis job.
-        return
+
     with tempfile.TemporaryDirectory() as tempdir:
         log.debug(f'Working in {tempdir}')
+        conf = straxen.contexts.xnt_common_config
+        conf['gain_model'] = ('to_pe_constant', 0.01)
+        conf_override = test_load_nt()
         st = strax.Context(
             storage=tempdir,
             config=dict(
@@ -58,7 +59,8 @@ def test_sim_nT():
                 detector='XENONnT',
                 fax_config=('https://raw.githubusercontent.com/XENONnT/WFSim'
                 '/22004e28421044452f42aaf5797be7186e07bbba/files/XENONnT_wfsim_config.json'),
-                **straxen.contexts.xnt_common_config),
+                **conf,
+                fax_config_override=conf_override),
             **straxen.contexts.common_opts)
         st.register(wfsim.RawRecordsFromFaxNT)
 
