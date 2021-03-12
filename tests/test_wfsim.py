d653a287dd1fbbdb44ff78fb034dde66c0193a9d
Joran Angevaare <jorana@nikhef.nl>
2021-03-12 16:21:44 +0100

    update 1T to again run (whoops)

--- tests/test_wfsim.py
+++ tests/test_wfsim.py
@@ -16,32 +16,32 @@ strax.mailbox.Mailbox.DEFAULT_TIMEOUT = 60
 run_id = '010000'
 
 
-# def test_sim_1T():
-#     """Test the 1T simulator (should always work with the publicly available files)"""
-#     with tempfile.TemporaryDirectory() as tempdir:
-#         log.debug(f'Working in {tempdir}')
-#         testing_config_1T = dict(
-#             hev_gain_model=('to_pe_constant', 0.0085),
-#             gain_model=('to_pe_constant', 0.0085)
-#         )
-#         st = strax.Context(
-#             storage=tempdir,
-#             config=dict(
-#                 nchunk=1, event_rate=1, chunk_size=2,
-#                 detector='XENON1T',
-#                 fax_config=('https://raw.githubusercontent.com/XENONnT/strax_auxiliary_files'
-#                     '/1d3178a375b379d5e7be05dda7831b481cd29a24/sim_files/fax_config_1t.json'),  # noqa
-#                 **straxen.contexts.x1t_common_config),
-#             **straxen.contexts.common_opts)
-#         st.register(wfsim.RawRecordsFromFax1T)
-#         log.debug(f'Setting testing config {testing_config_1T}')
-#         st.set_config(testing_config_1T)
-#
-#         log.debug(f'Getting raw-records')
-#         rr = st.get_array(run_id, 'raw_records')
-#         p = st.get_array(run_id, 'peaks')
-#         _sanity_check(rr, p)
-#         log.info(f'All done')
+def test_sim_1T():
+    """Test the 1T simulator (should always work with the publicly available files)"""
+    with tempfile.TemporaryDirectory() as tempdir:
+        log.debug(f'Working in {tempdir}')
+        testing_config_1T = dict(
+            hev_gain_model=('to_pe_constant', 0.0085),
+            gain_model=('to_pe_constant', 0.0085)
+        )
+        st = strax.Context(
+            storage=tempdir,
+            config=dict(
+                nchunk=1, event_rate=1, chunk_size=2,
+                detector='XENON1T',
+                fax_config=('https://raw.githubusercontent.com/XENONnT/strax_auxiliary_files'
+                    '/1d3178a375b379d5e7be05dda7831b481cd29a24/sim_files/fax_config_1t.json'),  # noqa
+                **straxen.contexts.x1t_common_config),
+            **straxen.contexts.common_opts)
+        st.register(wfsim.RawRecordsFromFax1T)
+        log.debug(f'Setting testing config {testing_config_1T}')
+        st.set_config(testing_config_1T)
+
+        log.debug(f'Getting raw-records')
+        rr = st.get_array(run_id, 'raw_records')
+        p = st.get_array(run_id, 'peaks')
+        _sanity_check(rr, p)
+        log.info(f'All done')
 
 
 def test_sim_nT():
