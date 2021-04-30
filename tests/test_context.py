d88be67e4a51fb0237f44b4bbbf3ca4e7dca69f7
Joran Angevaare <jorana@nikhef.nl>
2021-04-30 10:49:23 +0200

    make it even more general

--- tests/test_context.py
+++ tests/test_context.py
@@ -3,19 +3,26 @@ import straxen
 import wfsim
 
 
-def test_nt_context(register=None):
+def test_nt_context(register=None, context=None):
     """
     Test a context if it is properly setup. To this end, we perform a
     simple scan of the field "time" since all plugins should have this
     field, if there is some option specified badly, we will quickly find
     out since for this scan, we activate all the plugins.
     :param register: Register a plugin (optional)
+    :param context: Test with some other context than the nT simulation
+    context
     """
-    st = straxen.contexts.xenonnt_simulation()
+    if context is None:
+        context = straxen.contexts.xenonnt_simulation()
+    assert isinstance(context, strax.Context), f'{context} is not a context'
+
     if register is not None:
         assert issubclass(register, strax.Plugin), f'{register} is not a plugin'
-        st.register(register)
-    st.search_field('time')
+        context.register(register)
+
+    # Search all plugins for the time field (each should have one)
+    context.search_field('time')
 
 
 def test_mc_chain():
@@ -26,8 +33,5 @@ def test_fax_nveto():
     test_nt_context(wfsim.RawRecordsFromFaxnVeto)
 
 
-def test_1t_context(register=None):
-    st = straxen.contexts.xenon1t_simulation()
-    if register is not None:
-        st.register(register)
-    st.search_field('time')
+def test_1t_context():
+    test_nt_context(context=straxen.contexts.xenon1t_simulation())
