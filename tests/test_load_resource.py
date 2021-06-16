61036b4a02c163f1a97712df4626c205cb46f753
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2021-06-16 16:56:10 +0900

    Add template for test

--- tests/test_load_resource.py
+++ tests/test_load_resource.py
@@ -16,8 +16,33 @@ def test_load_1t():
     result = load_config(config)
     return result, config
 
-
 def test_load_nt():
+    config = {
+        "detector": "XENONnT",
+        "s2_luminescence_model": "simple",
+        "enable_gas_gap_warping": False,
+        "enable_pmt_afterpulses": False,
+        "enable_electron_afterpulses": False,
+        "enable_noise": False,
+        "field_distortion_on": False,
+        "enable_field_dependencies": {
+            "survival_probability_map": True,
+            "drift_speed_map": False,
+            "diffusion_longitudinal_map": False,
+            "diffusion_radial_map": False,
+            "diffusion_azimuthal_map": False},
+        "neutron_veto": False,
+        "url_base": "https://raw.githubusercontent.com/XENONnT/WFSim/9e6ecfab13a314a83eec9844ba40811bc4a2dc36/files",
+        "photon_area_distribution": "XENONnT_spe_distributions_single_channel.csv",
+        "s1_pattern_map": ["constant dummy", 14e-5, [494,]],
+        "s2_pattern_map": ["constant dummy", 30e-5, [494,]],
+        "s2_correction_map": ["constant dummy", 1, []],
+        "field_dependencies_map": ["constant dummy", 1, []],
+    }
+    result = load_config(config)
+    return result, config
+
+def test_load_nt_mc_chain():
     config = {
         "detector": "XENONnT",
         "s2_luminescence_model": "simple",
