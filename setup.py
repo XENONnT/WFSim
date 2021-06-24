fafcf567590d68e2f1d52c1c36030fd0058ecb43
jorana <jorana@nikhef.nl>
2021-06-24 09:47:53 +0200

    Bump version: 0.5.3 → 0.5.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('HISTORY.md') as file:
 
 setuptools.setup(
     name='wfsim',
-    version='0.5.3',
+    version='0.5.4',
     description='XENONnT Waveform simulator',
     author='Wfsim contributors, the XENON collaboration',
     url='https://github.com/XENONnT/wfsim',
